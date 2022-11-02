8064045cbb1b289f9c757feaaee57a3a7fe71e11
daniel <danielkalinov@protonmail.com>
2022-11-02 12:39:41 +0200

    added storage and download url to response

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,30 @@
 import express from 'express';
-import { createReadStream, stat } from 'fs';
-import { promisify } from 'util';
+import { initializeApp } from 'firebase/app';
+import { getDownloadURL, getStorage, ref } from 'firebase/storage';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const firebaseConfig = {
+	apiKey: 'AIzaSyADMil1RpvMgqGyM9S46vVgsYTegoLx8oM',
+	authDomain: 'mern-music-streaming-9eace.firebaseapp.com',
+	projectId: 'mern-music-streaming-9eace',
+	storageBucket: 'mern-music-streaming-9eace.appspot.com',
+	messagingSenderId: '99024871709',
+	appId: '1:99024871709:web:e3018dc0cae624946741f4',
+};
+
+const firebase = initializeApp(firebaseConfig);
+const storage = getStorage(firebase);
+
 const port = process.env.PORT || 3000;
 const app = express();
-
-const filename = './test_audio.mp3';
-const fileInfo = promisify(stat);
-
-app.get('/', (req, res) => res.send('Hello World'));
+app.use(cors());
 
 app.get('/test_audio', async (req, res) => {
-	const { size } = await fileInfo(filename);
-	const range = req.headers.range;
-	if (range) {
-		let [start, end]: number[] | string[] = range
-			.replace(/bytes=/, '')
-			.split('-');
-		start = parseInt(start, 10);
-		end = end ? parseInt(end, 10) : size - 1;
+	const url = await getDownloadURL(ref(storage, 'music/test_audio.mp3'));
 
-		res.writeHead(206, {
-			'Content-Range': `bytes ${start}-${end}/${size}`,
-			'Accept-Ranges': 'bytes',
-			'Content-Length': start - end + 1,
-			'Content-Type': 'audio/mp3',
-		});
-
-		createReadStream(filename, { start, end }).pipe(res);
-	} else {
-		res.writeHead(200, {
-			'Content-Type': 'audio/mp3',
-			'Content-Length': size,
-		});
-
-		createReadStream(filename).pipe(res);
-	}
+	res.send(url);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
