ef6b0073531d09d5910e48bea396ca039f9a1b5d
Daniel Kalinov <danielkalinov@protonmail.com>
2024-05-11 17:00:45 +0300

    added playlist model

---
+++ src/models/playlist.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose';
+
+const ObjectId = Schema.Types.ObjectId;
+
+const playlistSchema = new Schema(
+  {
+    name: String,
+    imageUrl: String,
+    tracks: [{ _id: ObjectId, track: { type: ObjectId, ref: 'track' } }],
+  },
+  { versionKey: false }
+);
+
+const Playlist = mongoose.model('playlist', playlistSchema);
+
+export default Playlist;
